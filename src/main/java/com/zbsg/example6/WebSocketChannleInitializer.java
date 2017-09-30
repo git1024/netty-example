7b0bb2d9e6f6bb4dafbcf6b5461744c2ae6c381a
yaoyl <yaoyl@zbsg.com.cn>
2017-09-30 11:05:55 +0800

    add webSocket example

--- src/main/java/com/zbsg/example6/WebSocketChannleInitializer.java
+++ src/main/java/com/zbsg/example6/WebSocketChannleInitializer.java
@@ -1,6 +1,5 @@
 package com.zbsg.example6;
 
-import com.zbsg.example5.server.BeatServerHandler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
@@ -8,9 +7,6 @@ import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
-import io.netty.handler.timeout.IdleStateHandler;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by yaoyuanliang on 2017/9/22.
@@ -24,6 +20,6 @@ public class WebSocketChannleInitializer extends ChannelInitializer<SocketChanne
         pipeline.addLast(new HttpObjectAggregator(8192));       // Aggregator 聚合
         pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));         //webSocket服务器协议处理器
 
-
+        pipeline.addLast(new TextWebSocketFrameHandler());
     }
 }
